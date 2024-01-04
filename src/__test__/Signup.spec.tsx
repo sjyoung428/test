74f1e95324d1a1ede4a4dd9315e2a8bee4bc15fc
sjyoung428 <sjy8267@naver.com>
2024-01-04 16:46:49 +0900

    signup fail test

--- src/__test__/Signup.spec.tsx
+++ src/__test__/Signup.spec.tsx
@@ -1,3 +1,45 @@
+import "@testing-library/jest-dom";
+import { fireEvent, render, screen } from "@testing-library/react";
+import SignupPage from "../pages/SignupPage";
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {},
+});
+
 describe("회원가입 테스트", () => {
-  test("환경설정 테스트", () => {})
-})
+  test("비밀번호와 비밀번호 확인 값이 일치하지 않으면 에러메세지가 표시된다.", async () => {
+    // given - 회원가입 페이지가 그려짐
+    const routes = [
+      {
+        path: "/signup",
+        element: <SignupPage />,
+      },
+    ];
+
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/signup"],
+      initialIndex: 0,
+    });
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    );
+
+    // when - 비밀번호와 비밀번호 확인 값이 일치하지 않음
+    const passwordInput = screen.getByLabelText("비밀번호");
+    const passwordConfirmInput = screen.getByLabelText("비밀번호 확인");
+
+    fireEvent.change(passwordInput, { target: { value: "password" } });
+    fireEvent.change(passwordConfirmInput, {
+      target: { value: "password123" },
+    });
+
+    // then - 에러메세지가 표시됨
+    const errorMessage = await screen.findByTestId("error-message");
+    expect(errorMessage).toBeInTheDocument();
+  });
+});
